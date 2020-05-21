db1e1df11288921072b0621d2656c288f12981c7
puria2002 <59424461+puria2002@users.noreply.github.com>
2020-05-21 01:43:56 -0500

    Add files via upload

---
+++ BrokenKeyBoard.cpp
@@ -0,0 +1,40 @@
+//solution for UVa 11988
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main(void)
+{
+   list<char> text;
+   char c;
+   list<char>::iterator pos;
+   ios::sync_with_stdio(false);
+   while (!cin.eof())
+   {
+       pos = text.end();
+       while (cin.get(c), c != '\n') 
+       {
+           if (c == '[')
+           { 
+               pos = text.begin();
+               continue;
+           }
+           if (c == ']')
+           {
+               pos = text.end();
+               continue;
+           }
+           text.insert(pos, c); 
+
+       } 
+       for (char s : text)
+       {
+           std::cout << s;
+       }
+       std::cout << endl;
+       text.clear();
+        
+   }
+   return 0;
+} 
+
